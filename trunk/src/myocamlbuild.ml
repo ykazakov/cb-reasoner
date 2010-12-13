b6cdeb8d0c94abe99d01c9f1009bc38090fe08ef
ykazakovgo@gmail.com <ykazakovgo@gmail.com@d0210c74-2367-11de-85c7-557234b16fc6>
2010-12-13 20:25:05 +0000

    1. removed dependency on camlp4orf binary (may not be present in standard debian distribution of ocaml) 2. updated make files so that it is possible to cross-compile using mingw32-ocaml

--- trunk/src/myocamlbuild.ml
+++ trunk/src/myocamlbuild.ml
@@ -4,8 +4,8 @@ open Command;;
 
 dispatch begin function
 		| After_rules ->
-			  (* custom camlp4 preprocessing files *)
-				flag ["ocaml"; "pp"; "use_typeext"] (A"libs/pa_typeext.cmo");
+		    (* custom camlp4 preprocessing files *)
+				flag ["ocaml"; "pp"; "use_typeext"] & S[A"-parser"; A"Camlp4OCamlRevisedParser"; A"-parser"; A"Camlp4OCamlParser"; A"libs/pa_typeext.cmo"];
 				dep ["ocaml"; "ocamldep"; "use_typeext"] ["libs/pa_typeext.cmo"];
 		| _ -> ()
 	end;;
